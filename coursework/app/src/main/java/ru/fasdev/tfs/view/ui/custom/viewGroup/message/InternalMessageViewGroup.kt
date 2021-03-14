a0873e32e937c4a106882e5829d9b4776b3316b6
Andrey Rednikov <andreyrednikov@gmail.com>
2021-03-14 20:52:00 +0300

    Added InternalMessageViewGroup

--- coursework/app/src/main/java/ru/fasdev/tfs/view/ui/custom/viewGroup/message/InternalMessageViewGroup.kt
+++ coursework/app/src/main/java/ru/fasdev/tfs/view/ui/custom/viewGroup/message/InternalMessageViewGroup.kt
@@ -1,50 +1,104 @@
 package ru.fasdev.tfs.view.ui.custom.viewGroup.message
 
 import android.content.Context
+import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.TextView
 import ru.fasdev.tfs.R
+import ru.fasdev.tfs.view.feature.util.getHeightMeasuredMargin
+import ru.fasdev.tfs.view.feature.util.getWidthMeasuredMargin
+import ru.fasdev.tfs.view.feature.util.layout
 import ru.fasdev.tfs.view.ui.custom.layout.FlexBoxLayout
 
-/*
 class InternalMessageViewGroup
     @JvmOverloads constructor(
             context: Context,
-            attrs: AttributeSet? = null,
+            attributeSet: AttributeSet? = null,
             defStyleAttr: Int = 0,
             defStyleRes: Int = 0
-    ) : SimpleMessageViewGroup(context, attrs, defStyleAttr, defStyleRes)
+    ) : MessageViewGroupRoot(context, attributeSet, defStyleAttr, defStyleRes)
 {
-    override val msgTextView: TextView
-    override val reactionLayout: FlexBoxLayout
+    //#region View
+    override val messageTextView: TextView
+    override val reactionsLayout: FlexBoxLayout
+    //#endregion
+
+    //#region View Layout Params
+    private val messageLayoutParams: MarginLayoutParams
+        get() = messageTextView.layoutParams as MarginLayoutParams
+
+    private val reactionsLayoutParams: MarginLayoutParams
+        get() = reactionsLayout.layoutParams as MarginLayoutParams
+    //#endregion
+
+    //#region Rectangles Position View
+    private val messageRect = Rect()
+    private val reactionsRect = Rect()
+    //#endregion
 
     init {
         LayoutInflater.from(context).inflate(R.layout.view_internal_message, this, true)
 
-        msgTextView = findViewById(R.id.message_text)
-        reactionLayout = findViewById(R.id.reaction_layout)
+        messageTextView = findViewById(R.id.message_text)
+        reactionsLayout = findViewById(R.id.reaction_layout)
 
-        context.obtainStyledAttributes(attrs, R.styleable.InternalMessageViewGroup).apply {
-            msgText = getString(R.styleable.InternalMessageViewGroup_msgText) ?: ""
-            msgViewMaxSize = getMsgViewMaxSize()
+        context.obtainStyledAttributes(attributeSet, R.styleable.InternalMessageViewGroup).apply {
+            message = getString(R.styleable.InternalMessageViewGroup_mvMessage) ?: ""
+            reactions = listOf()
 
             recycle()
         }
-
-        updateReactionLayout()
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val maxWidth = MeasureSpec.getSize(widthMeasureSpec)
+
+        //#region Measure Message Text Size
+        val occupiedWidthSpace = maxWidth - MAX_MESSAGE_WIDTH
+        measureChildWithMargins(
+                messageTextView,
+                widthMeasureSpec, occupiedWidthSpace,
+                heightMeasureSpec, 0
+        )
+
+        val messageHeight = messageTextView.getHeightMeasuredMargin()
+        val messageWidth = messageTextView.getWidthMeasuredMargin()
+        //#endregion
+
+        //#region Measure Reaction Layout Size
+        measureChildWithMargins(
+                reactionsLayout,
+                widthMeasureSpec, 0,
+                heightMeasureSpec, messageHeight
+        )
+        val reactionHeight = if (reactionsLayout.childCount == 0) 0
+            else reactionsLayout.getHeightMeasuredMargin()
+        //#endregion
+
+        //#region Calculate Size
+        val width = messageWidth
+        val height = messageHeight + reactionHeight
+        //#endregion
+
+        val resolveWidth = resolveSize(width, widthMeasureSpec)
+        val resolveHeight = resolveSize(height, heightMeasureSpec)
+
+        setMeasuredDimension(resolveWidth, resolveHeight)
     }
 
-    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int)
-    {
-        TODO("Not yet implemented")
-    }
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+        messageRect.top = messageLayoutParams.topMargin
+        messageRect.bottom = messageRect.top + messageTextView.measuredHeight
+        messageRect.right = measuredWidth
+        messageRect.left = messageRect.right - messageTextView.measuredWidth
 
-    override fun generateDefaultLayoutParams() = MarginLayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
-    override fun generateLayoutParams(attrs: AttributeSet) = MarginLayoutParams(context, attrs)
-    override fun generateLayoutParams(p: LayoutParams) = MarginLayoutParams(p)
-}*/
+        reactionsRect.top = messageRect.bottom + reactionsLayoutParams.topMargin
+        reactionsRect.bottom = reactionsRect.top + reactionsLayout.measuredHeight
+        reactionsRect.right = messageRect.right
+        reactionsRect.left = reactionsRect.right - reactionsLayout.measuredWidth
+
+        messageTextView.layout(messageRect)
+        reactionsLayout.layout(reactionsRect)
+    }
+}
