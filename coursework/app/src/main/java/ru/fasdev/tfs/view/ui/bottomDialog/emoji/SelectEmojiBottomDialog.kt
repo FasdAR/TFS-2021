b2e16eda20019c87412bf5dfa876ad59bebde172
Andrey Rednikov <andreyrednikov@gmail.com>
2021-03-16 20:41:55 +0300

    Added EmojiUtil

--- coursework/app/src/main/java/ru/fasdev/tfs/view/ui/bottomDialog/emoji/SelectEmojiBottomDialog.kt
+++ coursework/app/src/main/java/ru/fasdev/tfs/view/ui/bottomDialog/emoji/SelectEmojiBottomDialog.kt
@@ -1,10 +1,10 @@
 package ru.fasdev.tfs.view.ui.bottomDialog.emoji
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,28 +12,16 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import ru.fasdev.tfs.databinding.BottomDialogSelectEmojiBinding
 import ru.fasdev.tfs.view.feature.recycler.Adapter
 import ru.fasdev.tfs.view.feature.recycler.base.ViewTyped
+import ru.fasdev.tfs.view.feature.util.EmojiUtil
 import ru.fasdev.tfs.view.feature.util.toDp
-import ru.fasdev.tfs.view.feature.util.toSp
 import ru.fasdev.tfs.view.ui.bottomDialog.emoji.adapter.EmojiHolderFactory
 import ru.fasdev.tfs.view.ui.bottomDialog.emoji.adapter.viewTypes.EmojiUi
-import java.util.regex.Matcher
-import java.util.regex.Pattern
+import kotlin.concurrent.thread
 
 class SelectEmojiBottomDialog : BottomSheetDialogFragment()
 {
     companion object {
         const val TAG = "BS_SELECT_EMOJI"
-        const val SUPPORTED_EMOJI = "\uD83D\uDE0E\uD83D\uDE0C\uD83D\uDE0C\uD83D\uDE14\uD83D\uDE0B" +
-                "\uD83D\uDE1B\uD83D\uDE0D\uD83D\uDE01\uD83D\uDC4D\uD83D\uDE18\uD83C\uDF83\uD83D" +
-                "\uDE0A\uD83D\uDE1A\uD83D\uDE43\uD83D\uDE05\uD83D\uDE02\uD83D\uDE07\uD83D\uDE09" +
-                "\uD83D\uDE0C\uD83D\uDE17\uD83D\uDE1C\uD83D\uDE0E\uD83D\uDE0F\uD83D\uDE1C\uD83D" +
-                "\uDE17\uD83D\uDE17\uD83D\uDE02\uD83D\uDE0E\uD83D\uDE0C\uD83D\uDE0C\uD83D\uDE14" +
-                "\uD83D\uDE0B\uD83D\uDE1B\uD83D\uDE0D\uD83D\uDE01\uD83D\uDC4D\uD83D\uDE18\uD83C" +
-                "\uDF83\uD83D\uDE0A\uD83D\uDE1A\uD83D\uDE43\uD83D\uDE05\uD83D\uDE02\uD83D\uDE07" +
-                "\uD83D\uDE09\uD83D\uDE0C\uD83D\uDE17\uD83D\uDE1C\uD83D\uDE0E\uD83D\uDE0F\uD83D" +
-                "\uDE1C\uD83D\uDE17\uD83D\uDE17\uD83D\uDE02"
-
-        const val EMOJI_REGEX = "[\uD83C-\uDBFF\uDC00-\uDFFF]+"
 
         fun newInstance() = SelectEmojiBottomDialog()
         fun show(fragmentManager: FragmentManager): SelectEmojiBottomDialog =
@@ -54,21 +42,20 @@ class SelectEmojiBottomDialog : BottomSheetDialogFragment()
         super.onViewCreated(view, savedInstanceState)
 
         val rvList: RecyclerView = binding.rvEmojiList
+        rvList.adapter = adapter
 
-        val result = Pattern.compile("\\P{M}\\p{M}*+").matcher(SUPPORTED_EMOJI)
+        thread {
+            val listEmoji = EmojiUtil.getListEmoji(requireContext())
+            val resultEmoji = listEmoji.map { EmojiUi(listEmoji.indexOf(it), it) }
 
-        val matchList: MutableList<String> = mutableListOf()
-        while (result.find()) {
-            matchList.add(result.group())
+            rvList.post {
+                adapter.items = resultEmoji
+            }
         }
 
-        val resL = matchList.map { EmojiUi(matchList.indexOf(it), it) }
-        adapter.items = resL
-
         rvList.post {
-            val sizeColumn = rvList.width / 55.toDp
+            val sizeColumn = rvList.width / 55.toDp // Calculate column size
             rvList.layoutManager = GridLayoutManager(context, sizeColumn)
-            rvList.adapter = adapter
         }
     }
 
