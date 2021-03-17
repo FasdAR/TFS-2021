9d4055db3734deab65553b126f77b69813665ef1
Andrey Rednikov <andreyrednikov@gmail.com>
2021-03-17 19:39:07 +0300

    Edited import

--- coursework/app/src/main/java/ru/fasdev/tfs/view/ui/fragment/chat/ChatFragment.kt
+++ coursework/app/src/main/java/ru/fasdev/tfs/view/ui/fragment/chat/ChatFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.setFragmentResultListener
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import ru.fasdev.tfs.R
