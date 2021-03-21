3f7b0e1ca8c8a49c9447ac1c86f6a7735b216eb9
Andrey Rednikov <andreyrednikov@gmail.com>
2021-03-21 20:06:07 +0300

    Added ProfileFragment layout

--- coursework/app/src/main/java/ru/fasdev/tfs/view/ui/fragment/profile/ProfileFragment.kt
+++ coursework/app/src/main/java/ru/fasdev/tfs/view/ui/fragment/profile/ProfileFragment.kt
@@ -1,12 +1,39 @@
 package ru.fasdev.tfs.view.ui.fragment.profile
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import ru.fasdev.tfs.R
+import ru.fasdev.tfs.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment(R.layout.fragment_profile)
 {
     companion object {
         val TAG: String = ProfileFragment::class.java.simpleName
         fun newInstance(): ProfileFragment = ProfileFragment()
+    }
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = super.onCreateView(inflater, container, savedInstanceState)
+        view?.let { _binding = FragmentProfileBinding.bind(it) }
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.layoutInfo.avatar.setImageResource(R.drawable.ic_launcher_background)
+        binding.layoutInfo.fullName.text = "Andrey Rednikov"
+        binding.layoutInfo.status.text = resources.getString(R.string.status_meeting)
+        binding.layoutInfo.onlineStatus.text = resources.getString(R.string.online)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
