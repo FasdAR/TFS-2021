c1c6169cc0024cdf42d3dd32d26705ab44239c92
Andrey Rednikov <andreyrednikov@gmail.com>
2021-03-23 23:02:20 +0300

    Fixed uId Collision in BaseDiffUtilCallback

--- coursework/app/src/main/java/ru/fasdev/tfs/view/ui/global/recycler/base/BaseDiffUtilCallback.kt
+++ coursework/app/src/main/java/ru/fasdev/tfs/view/ui/global/recycler/base/BaseDiffUtilCallback.kt
@@ -3,7 +3,12 @@ package ru.fasdev.tfs.view.ui.global.recycler.base
 import androidx.recyclerview.widget.DiffUtil
 
 open class BaseDiffUtilCallback<T : ViewType> : DiffUtil.ItemCallback<T>() {
-    override fun areItemsTheSame(oldItem: T, newItem: T): Boolean = oldItem.uId == newItem.uId
+    override fun areItemsTheSame(oldItem: T, newItem: T): Boolean {
+        if (oldItem::class.simpleName == newItem::class.simpleName)
+            return oldItem.uId == newItem.uId
+
+        return false
+    }
 
     override fun areContentsTheSame(oldItem: T, newItem: T): Boolean = oldItem == newItem
 }
