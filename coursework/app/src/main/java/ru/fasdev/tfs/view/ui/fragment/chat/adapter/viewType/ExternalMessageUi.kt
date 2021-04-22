2d917304e79914e2a1734923d008512fc9e48b28
Andrey Rednikov <andreyrednikov@gmail.com>
2021-04-22 23:33:59 +0300

    Fix data class

--- coursework/app/src/main/java/ru/fasdev/tfs/view/ui/fragment/chat/adapter/viewType/ExternalMessageUi.kt
+++ coursework/app/src/main/java/ru/fasdev/tfs/view/ui/fragment/chat/adapter/viewType/ExternalMessageUi.kt
@@ -3,7 +3,8 @@ package ru.fasdev.tfs.view.ui.fragment.chat.adapter.viewType
 import ru.fasdev.tfs.R
 import ru.fasdev.tfs.view.ui.global.view.viewGroup.message.model.MessageReactionUi
 
-class ExternalMessageUi(
+//Здесь дата класс для генерации equals and hashcode
+data class ExternalMessageUi(
     override val uId: Int,
     val nameSender: String,
     val avatarSrc: String,
--- coursework/app/src/main/java/ru/fasdev/tfs/view/ui/fragment/chat/adapter/viewType/InternalMessageUi.kt
+++ coursework/app/src/main/java/ru/fasdev/tfs/view/ui/fragment/chat/adapter/viewType/InternalMessageUi.kt
@@ -3,7 +3,8 @@ package ru.fasdev.tfs.view.ui.fragment.chat.adapter.viewType
 import ru.fasdev.tfs.R
 import ru.fasdev.tfs.view.ui.global.view.viewGroup.message.model.MessageReactionUi
 
-class InternalMessageUi(
+//Здесь дата класс для генерации equals and hashcode
+data class InternalMessageUi(
     override val uId: Int,
     override val message: String,
     override val reactions: List<MessageReactionUi>,
